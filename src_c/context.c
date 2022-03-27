e7e8bbe371ed0bcf9750508edadbd63cc64fbd84
Ankith <46915066+ankith26@users.noreply.github.com>
2022-03-27 19:15:58 +0530

    Add `get_pref_locales`

--- src_c/context.c
+++ src_c/context.c
@@ -10,25 +10,94 @@ pg_context_get_pref_path(PyObject *self, PyObject *args, PyObject *kwargs)
     char *org, *project;
     static char *kwids[] = {"org", "app", NULL};
 
-    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "ss", kwids, &org, &project)) {
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "ss", kwids, &org,
+                                     &project)) {
         return NULL;
     }
 
-    char* path = SDL_GetPrefPath(org, project);
+    char *path = SDL_GetPrefPath(org, project);
     if (path == NULL) {
         return RAISE(pgExc_SDLError, SDL_GetError());
     }
 
-    PyObject* ret = Py_BuildValue("s", path);
+    PyObject *ret = Py_BuildValue("s", path);
     SDL_free(path);
 
     return ret;
 }
 
+static PyObject *
+pg_context_get_pref_locales(PyObject *self, PyObject *_null)
+{
+    PyObject *ret_list = PyList_New(0);
+    if (!ret_list) {
+        return NULL;
+    }
+
+#if SDL_VERSION_ATLEAST(2, 0, 14)
+    PyObject *dict, *string = NULL;
+    SDL_Locale *locales = SDL_GetPreferredLocales();
+    if (!locales) {
+        /* Return an empty list if SDL function does not return any useful
+         * information */
+        return ret_list;
+    }
+
+    SDL_Locale *current_locale = locales;
+    while (current_locale->language) {
+        dict = PyDict_New();
+        if (!dict) {
+            goto error;
+        }
+
+        string = PyUnicode_FromString(current_locale->language);
+        if (!string) {
+            goto error;
+        }
+        if (PyDict_SetItemString(dict, "language", string)) {
+            goto error;
+        }
+        Py_DECREF(string);
+
+        if (current_locale->country) {
+            string = PyUnicode_FromString(current_locale->country);
+            if (!string) {
+                goto error;
+            }
+            if (PyDict_SetItemString(dict, "country", string)) {
+                goto error;
+            }
+            Py_DECREF(string);
+        }
+
+        /* reset string to NULL because goto XDECREF's this */
+        string = NULL;
+        if (PyList_Append(ret_list, dict)) {
+            goto error;
+        }
+        Py_DECREF(dict);
+        current_locale++;
+    }
+
+    SDL_free(locales);
+    return ret_list;
+error:
+    Py_XDECREF(string);
+    Py_XDECREF(dict);
+    SDL_free(locales);
+    Py_DECREF(ret_list);
+    return NULL;
+#else
+    return ret_list;
+#endif
+}
+
 static PyMethodDef _context_methods[] = {
-    {"get_pref_path", pg_context_get_pref_path, METH_VARARGS | METH_KEYWORDS, DOC_PYGAMECONTEXTGETPREFPATH},
-    {NULL, NULL, 0, NULL}
-};
+    {"get_pref_path", pg_context_get_pref_path, METH_VARARGS | METH_KEYWORDS,
+     DOC_PYGAMECONTEXTGETPREFPATH},
+    {"get_pref_locales", pg_context_get_pref_locales, METH_NOARGS,
+     DOC_PYGAMECONTEXTGETPREFLOCALES},
+    {NULL, NULL, 0, NULL}};
 
 MODINIT_DEFINE(context)
 {
--- test/context_test.py
+++ test/context_test.py
@@ -1,6 +1,7 @@
 import unittest
 import pygame
 
+
 class ContextModuleTest(unittest.TestCase):
 
     # how do you test something that creates a folder?
@@ -9,7 +10,32 @@ class ContextModuleTest(unittest.TestCase):
     def test_get_pref_path(self):
         pass
 
-    
+    def test_get_pref_locales(self):
+        locales = pygame.context.get_pref_locales()
+
+        # check type of return first
+        self.assertIsInstance(locales, list)
+        for lc in locales:
+            self.assertIsInstance(lc, dict)
+            lang = lc.get("language", None)
+            self.assertIsInstance(lang, str)
+
+            # length of language code should be greater than 1
+            self.assertTrue(len(lang) > 1)
+
+            try:
+                # country field is optional
+                country = lc["country"]
+                self.assertIsInstance(country, str)
+
+                # length of country code should be greater than 1
+                self.assertTrue(len(country) > 1)
+            except KeyError:
+                pass
+
+        # passing args should raise error
+        for arg in (None, 1, "hello"):
+            self.assertRaises(TypeError, pygame.context.get_pref_locales, arg)
 
 
 if __name__ == "__main__":
